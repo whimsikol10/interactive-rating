e7d7b06a590902b02da8ed86585bf2707422979a
Kolten Schnack <kolten.schnack@gmail.com>
2022-05-14 20:47:49 -0500

    i tried my best and still doesn't work

--- index.js
+++ index.js
@@ -9,5 +9,33 @@ function changeButton(event) {
         btn.classList.remove('active');
     });
     event.target.classList.add('active');
-})
 };
+
+function displaySubmit() {
+    const hidden = document.getElementsByClassName('hide');
+
+    hidden.forEach(el => {
+        el.classList.remove('hide');
+
+    });
+
+    const radios = document.querySelectorAll('input');
+    radios.forEach(radio => {
+        radio.classList.add('hide');
+    });
+
+    const displayed = document.getElementsByClassName('show');
+
+    displayed.forEach(el => {
+        el.classList.add('hide');
+        el.classList.remove('show');
+    });
+
+    let rating = document.querySelector('input[name="rating"]:checked').value;
+
+    document.getElementById('feedback').innerText = `You selected ${rating} out of 5`;
+};
+
+const form = document.getElementById('ratingform');
+
+form.addEventListener('submit', displaySubmit);
